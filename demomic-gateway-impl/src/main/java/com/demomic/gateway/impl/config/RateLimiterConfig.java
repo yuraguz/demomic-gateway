496d1064ad00cbce405c01cfc22853e1929d68e6
yuraguz <yuraguz22@gmail.com>
2023-06-06 22:58:12 +0300

    Session management and rate limiter

---
+++ demomic-gateway-impl/src/main/java/com/demomic/gateway/impl/config/RateLimiterConfig.java
@@ -0,0 +1,15 @@
+package com.demomic.gateway.impl.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just("anonymus");
+    }
+}
